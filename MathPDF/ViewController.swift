418a4990953a780de1686082be998b06879e5f08
Zihong Lin <msasysu.lzh@icloud.com>
2026-04-04 23:26:11 -0700

    Initial Commit

---
+++ MathPDF/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MathPDF
+//
+//  Created by Zihong Lin on 4/4/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
