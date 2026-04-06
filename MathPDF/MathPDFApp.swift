6fab2881dc9737a658db74794ab4f94fdd80e112
ZihongLin <1046298030@qq.com>
2026-04-05 19:39:49 -0700

    Initial Commit

---
+++ MathPDF/MathPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathPDFApp.swift
+//  MathPDF
+//
+//  Created by Zihong Lin on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MathPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MathPDFTests/MathPDFTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathPDFTests.swift
+//  MathPDFTests
+//
+//  Created by Zihong Lin on 4/5/26.
+//
+
+import Testing
+@testable import MathPDF
+
+struct MathPDFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
